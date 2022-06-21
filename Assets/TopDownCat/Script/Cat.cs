a58609b32ae972ca62fd053ab77d807e44d38870
KangDHGit <eogks0323@gmail.com>
2022-06-21 14:30:02 +0900

    먹는 이모티콘 개별구현

--- Assets/TopDownCat/Script/Cat.cs
+++ Assets/TopDownCat/Script/Cat.cs
@@ -12,6 +12,8 @@ namespace MyCat
         Animator _anima;
         GameObject _target;
 
+        bool _isIdlePlay = false;   // 대기 애니매이션 랜덤재생중인가
+        float delay;
         //이모티콘 모음
         GameObject _emoteSweat;
         GameObject _emoteHappy;
@@ -29,12 +31,19 @@ namespace MyCat
             _emoteHappy.SetActive(false);
 
             // idle2, idle3 애니매이션 랜덤 재생 시작
-            float delay = Random.Range(3.0f, 5.0f);
-            Invoke("PlayIdle", delay);
+            //float delay = Random.Range(5.0f, 10.0f);
+            //Invoke("PlayRandomIdle", delay);
         }
 
-        void PlayIdle() // 대기 애니메이션 랜덤 재생
+        void PlayRandomIdle() // 대기 애니메이션 랜덤 재생
         {
+            // 움직이거나 먹는중이면 정지후 Bool 값 수정
+            if (_anima.GetBool("isMoving") || _anima.GetBool("isEating"))
+            {
+                _isIdlePlay = false;
+                return;
+            }
+
             int random = Random.Range(2, 4); // 2~3 랜덤
             if (random == 2)
                 _anima.SetTrigger("idle2");
@@ -48,13 +57,27 @@ namespace MyCat
             // 이항연산자 binary operator
             // 삼상연산자
 
-            float delay = Random.Range(3.0f, 10.0f);
-            Invoke("PlayIdle", delay);
+            float delay = Random.Range(5.0f, 10.0f);
+            Invoke("PlayRandomIdle", delay);
+            _isIdlePlay = true;
         }
         
-        // Update is called once per frame
+        void PlayIdleCheck() // 업데이트 함수에서 사용
+        {
+            // 대기 애니메이션 랜덤 재생중일시 리턴
+            if (_isIdlePlay == true)
+                return;
+            else if(_isIdlePlay == false)// 아니면 재생 후 Bool 값 수정
+            {
+                float delay = Random.Range(5.0f, 10.0f);
+                Invoke("PlayRandomIdle", delay);
+                _isIdlePlay = true;
+            }
+        }
+
         void Update()
         {
+            PlayIdleCheck();
         }
         private void FixedUpdate()
         {
@@ -69,6 +92,9 @@ namespace MyCat
             //Debug.Log("h: " + h);
             //Debug.Log(string.Format("v: {0}", v));
 
+            if (_anima.GetBool("isEating"))
+                return;
+
             float runSpeed = 1.0f;
             if (Input.GetKey(KeyCode.LeftShift))
             {
@@ -81,12 +107,11 @@ namespace MyCat
             float fixedDeltaTime = Time.fixedDeltaTime;
 
             _rigid.velocity = new Vector2(h, v).normalized * _speed * fixedDeltaTime * runSpeed;
-
-            //_anima.SetFloat("velocity", _rigid.velocity.magnitude);
-            
         }
         void Flip(float h)
         {
+            if(_anima.GetBool("isEating"))
+                return;
             if (h < 0)
             {
                 _renderer.flipX = false;
@@ -99,54 +124,12 @@ namespace MyCat
             _anima.SetFloat("Horizontal", h);
             _anima.SetFloat("Vertical", v);
             if(h == 0 && v == 0)
-                _anima.SetBool("ismoving", false);
+                _anima.SetBool("isMoving", false);
             else
-                _anima.SetBool("ismoving", true);
+                _anima.SetBool("isMoving", true);
             Flip(h);
         }
-        void MoveAnim(float h, float v)
-        {
-            if (h != 0 && v == 0)   // 좌우키
-            {
-                _anima.SetBool("ismovingLR", true);
-                _anima.SetBool("ismovingUp", false);
-                _anima.SetBool("ismovingDown", false);
-                Flip(h);
-            }
-            else if (h == 0 && v > 0)   // 상키
-            {
-                _anima.SetBool("ismovingUp", true);
-                _anima.SetBool("ismovingLR", false);
-                _anima.SetBool("ismovingDown", false);
-            }
-            else if (h == 0 && v < 0)   // 하키
-            {
-                _anima.SetBool("ismovingDown", true);
-                _anima.SetBool("ismovingLR", false);
-                _anima.SetBool("ismovingUp", false);
-            }
-
-            else if (h != 0 && v > 0)    // 좌우 + 상키
-            {
-                _anima.SetBool("ismovingUp", true);
-                _anima.SetBool("ismovingLR", true);
-                _anima.SetBool("ismovingDown", false);
-                Flip(h);
-            }
-            else if (h != 0 && v < 0)   // 좌우 + 하키
-            {
-                _anima.SetBool("ismovingDown", true);
-                _anima.SetBool("ismovingLR", true);
-                _anima.SetBool("ismovingUp", false);
-                Flip(h);
-            }
-            else
-            {
-                _anima.SetBool("ismovingLR", false);
-                _anima.SetBool("ismovingUp", false);
-                _anima.SetBool("ismovingDown", false);
-            }
-        }
+        
         private void OnCollisionEnter2D(Collision2D collision)
         {
             // 밥그릇 충돌시 밥는 애니메이션 및 이모티콘
@@ -180,6 +163,7 @@ namespace MyCat
             Debug.Log("StopEat and Happy");
         }
         // 밥 주기 관련--------------------------------
+
         // 타겟 이동 관련------------------------------
         public void SetTarget(GameObject target)
         {
